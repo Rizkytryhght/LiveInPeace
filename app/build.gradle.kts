ba45d3385d91f9f03e8c9ecb2ccb0f49ad77bd3d
Moch Rizky Taufiqurrahman <moch.rizky.tif23@polban.ac.id>
2025-05-14 13:03:11 +0700

    Update Fitur tes dass, Tambah halaman pengenalan dass

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     implementation ("androidx.room:room-runtime:2.6.1")
     kapt ("androidx.room:room-compiler:2.6.1")
     implementation ("androidx.room:room-ktx:2.6.1")
+    implementation ("androidx.navigation:navigation-compose:2.7.0")
 
     implementation(libs.mpandroidchart)
 
