b58c767e23134337500b07d16f22cfb810f28dcb
Naufal Asidiq <naufalasidiq150@gmail.com>
2025-05-24 23:38:48 +0700

    add confirm delete note

--- app/src/main/java/com/example/liveinpeace/ui/note/NoteActivity.kt
+++ app/src/main/java/com/example/liveinpeace/ui/note/NoteActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
@@ -44,6 +45,7 @@ class NoteActivity : AppCompatActivity() {
 
         noteViewModel = ViewModelProvider(this, viewModelFactory)[NoteViewModel::class.java]
 
+        // Di dalam NoteActivity, bagian adapter diubah jadi:
         adapter = NoteAdapter(mutableListOf(),
             { note ->
                 val intent = Intent(this@NoteActivity, NoteDetailActivity::class.java)
@@ -58,6 +60,7 @@ class NoteActivity : AppCompatActivity() {
             },
             { note ->
                 noteViewModel.deleteNote(note)
+                Toast.makeText(this, "Catatan dihapus", Toast.LENGTH_SHORT).show()
             }
         )
 
